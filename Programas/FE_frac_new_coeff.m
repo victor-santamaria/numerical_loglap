a8399282c89d898a2b6170d5555e41b48befb7c9
victor-santamaria <victor.santamaria@deusto.es>
2023-03-30 11:27:08 -0600

    subiendo version más actualizada de los programas

---
+++ Programas/FE_frac_new_coeff.m
@@ -0,0 +1,221 @@
+clear
+
+%%% Fractional power
+s=0.3;
+
+%%% Size of the domain \Omega=(-L,L)
+L=1;
+
+c_1 = 2*(s*2^(2*s-1)*gamma(0.5*(1+2*s)))/(sqrt(pi)*gamma(1-s));
+
+%%% Number of discrete points, mesh and mesh size
+Nval=[50,100,200,400,800,1600,3200,6400];
+dif_norm=[];
+dif_bilin=[];
+step=[];
+pendiente=[];
+
+for N=Nval
+
+    xi=linspace(-L,L,N+2);
+    h=xi(2)-xi(1);
+
+    As=flRigidity_w0(s,L,N);
+
+    %%% Right-hand side of the problem and projection over finite elements
+    f = @(x) 1+0*x;
+    %f = @(x) (c_1/(2*s))*((1+x).^(-2*s)+(1-x).^(-2*s));
+    %f = @(x) sin(pi*x);
+    F=projection(xi,f,h);
+
+    sol_frac=As\F;
+
+    %%% Exact solution
+    exsol=@(x,s,L) 2^(-2*s)*sqrt(pi)/(gamma((1+2*s)/2)*gamma(1+s)).*(L^2-x.^2).^s;
+    %exsol=@(x,s,L) 1+0.*x;
+
+    figure(1)
+    plot(xi, sol_frac,xi,exsol(xi,s,L),'x',xi,sol_frac*0,'LineWidth',1);
+    %plot(xi, sol_frac,'LineWidth',1);
+    
+    e_norm = error_fl(h,s,sol_frac);
+
+    e_bilin = error_bilinear(h,xi,s,sol_frac,f);
+
+    dif_norm=[dif_norm;e_norm];
+
+    dif_bilin=[dif_bilin;e_bilin];
+
+    step=[step;h]
+
+
+    if length(dif_norm)== 1
+        pendiente=[pendiente,NaN];
+    else
+        pendiente=[pendiente,log(dif_norm(end)/dif_norm(end-1))/log(step(end)/step(end-1))];
+    end
+
+end
+
+figure(3)
+loglog(step,dif_norm)
+sl_dif=log(dif_norm(1)/dif_norm(end))/log(step(1)/step(end));
+legend("Slope: "+num2str(sl_dif))
+
+
+
+%%% Auxiliary functions
+
+function [F] = projection(xi,f,h)
+
+N=size(xi,2);
+Phi = @(x) 1-abs(x);
+F = zeros(N,1);
+
+for i=1:N
+    if i~=1 && i~=N
+        xx = linspace(xi(i)-h,xi(i)+h,N+1);
+        xx = 0.5*(xx(2:end)+xx(1:end-1));
+        B1 = f(xx).*Phi((xx-xi(i))/h);
+        F(i) = ((2*h)/N)*sum(B1);
+    end
+
+    if i==1
+        xx = linspace(xi(i),xi(i)+h,N+1);
+        xx = 0.5*(xx(2:end)+xx(1:end-1));
+        B1 = 2*f(xx).*Phi((xx-xi(i))/h);
+        F(i) = ((h)/N)*sum(B1);
+    end
+
+    if i==N
+        xx = linspace(xi(i)-h,xi(i),N+1);
+        xx = 0.5*(xx(2:end)+xx(1:end-1));
+        B1 = 2*f(xx).*Phi((xx-xi(i))/h);
+        F(i) = ((h)/N)*sum(B1);
+    end
+
+end
+
+end
+
+
+function A = flRigidity_w0(s,L,N)
+
+x = linspace(-L,L,N+2);
+x = x(2:end-1);
+h = x(2)-x(1);
+
+A = zeros(N+2,N+2);
+
+c = (s*2^(2*s-1)*gamma(0.5*(1+2*s)))/(sqrt(pi)*gamma(1-s));
+
+% % %%% For taking into account the basis functions at the end sides
+%%% a(\phi_0,\phi_j) 
+for j=3:N+2
+    k = j;
+
+    den = 1/(4*s*(1-2*s));
+        p = 2-2*s;
+        q = 3-2*s;
+
+        B1=den*(2*k^(1-2*s)-((k+1)^p-(k-1)^p)/(1-s)...
+            -(2*k^q-(k+1)^q-(k-1)^q)/((1-s)*(3-2*s)));
+
+        B3=den*(-2*k^(1-2*s)+(2*k^p-2*(k-1)^p)/(1-s)...
+            +(2*(k-1)^q-k^q-(k-2)^q)/((1-s)*(3-2*s)));
+
+    if j==N+2
+        A(1,j) = (2*h^(1 - 2*s)*N^(-2*s)*(1 + N)^(-2*s)*(2 + N)^(-2*s)*(-N^(2*s)*(1 + N)^(2*s)*(2 + N)*...
+            (1 + (1 + N)^2 + 4*(1 + N)*(-1 + s) - 6*s +... 
+            4*s^2) + (2 + N)^(2*s)*(-N^3*(1 + N)^(2*s) + 2*N^(2*s)*(1 + N)^2*(-2 + N + 2*s))))/((-1 + s)*s*...
+            (-3 + 2*s)*(-1 + 2*s));
+     else
+        A(1,j) = -4*h^(1-2*s)*(B1+B3);
+    end
+    A(N+2,N+3-j)= A(1,j);
+end
+
+%%% Parece que aqui esta el problema
+%%% a(\phi_{0},\phi_{1}) & a(\phi_{N},\phi_{N+1})
+A(1,2)=-((2^(1 - 2*s)*(-2 + 4^s)*h^(1 - 2*s))/((-1 + s)*s*(-3 + 2*s)));
+A(N+2,N+1)=A(1,2);
+
+%%% The usual fractional laplacian matrix
+
+for i=2:N-1
+    for j=i+2:N+1
+        k = j-i;
+        den = 1/(4*s*(1-2*s));
+        p = 2-2*s;
+        q = 3-2*s;
+
+        B1=den*(2*k^(1-2*s)-((k+1)^p-(k-1)^p)/(1-s)...
+            -(2*k^q-(k+1)^q-(k-1)^q)/((1-s)*(3-2*s)));
+
+        B2=den*(-2*k^(1-2*s)+(2*(k+1)^p-2*k^p)/(1-s)...
+            +(2*(k+1)^q-k^q-(k+2)^q)/((1-s)*(3-2*s)));
+
+        B3=den*(-2*k^(1-2*s)+(2*k^p-2*(k-1)^p)/(1-s)...
+            +(2*(k-1)^q-k^q-(k-2)^q)/((1-s)*(3-2*s)));
+
+        B4=den*(2*k^(1-2*s)-((k+1)^p-(k-1)^p)/(1-s)...
+            -(2*k^q-(k+1)^q-(k-1)^q)/((1-s)*(3-2*s)));
+
+        A(i,j) = -2*h^(1-2*s)*(B1+B2+B3+B4);
+    end
+end
+
+for i=2:N
+    Q2=h^(1-2*s)*(2^p+2*s-3)/(s*(2-2*s)*(3-2*s));
+    Q3=h^(1-2*s)*(13-5*2^(3-2*s)+3^(3-2*s)+s*(2^(4-2*s)-14)+4*s^2)...
+        /(2*s*(1-2*s)*(1-s)*(3-2*s));
+    Q4=-(h^(1-2*s))/((1-s)*(3-2*s));
+    A(i,i+1) = 2*Q2+Q3+Q4;
+end
+
+A = A+A';
+
+for i=2:N+1
+    R2 = h^(1-2*s)*(4*s-6+2^(3-2*s))/(s*(1-2*s)*(1-s)*(3-2*s));
+    
+    R41 = h^(1-2*s)/((1-s)*(3-2*s));
+    R42 = h^(1-2*s)*((2*s^2-5*s+4-2^(2-2*s))/(s*(1-2*s)*(1-s)*(3-2*s)));
+
+    A(i,i) = 2*R2+2*(R41+R42);
+end
+ 
+
+%%% a(\phi_0,\phi_0) & a(\phi_{N+1},\phi_{N+1})
+A(1,1) = (8*h^(1 - 2*s))/(s*(3 + 4*(-2 + s)*s));
+A(N+2,N+2)=A(1,1);
+
+A = c*A;
+
+end
+
+function [e_norm] = error_fl(h,s,sol)
+
+val = pi/(2^(2*s)*gamma(s+0.5)*gamma(s+1.5));
+valnum = h*sum(sol);
+e_norm = sqrt(val-valnum);
+
+end
+
+function [e_norm] = error_bilinear(h,xi,s,sol,f)
+
+c_1 = 2*(s*2^(2*s-1)*gamma(0.5*(1+2*s)))/(sqrt(pi)*gamma(1-s));
+
+val = c_1*2^(1-2*s)/(s-2*s^2);
+
+numf=f(xi);
+numf=numf(2:end-1);
+
+valnum = h*sum(numf'.*sol(2:end-1));
+e_norm = sqrt(val-valnum);
+
+end
+
+
+
+
+
---
+++ Programas/FE_logarithmic_full_basis_noproj.m
@@ -0,0 +1,269 @@
+clear
+
+%%% Size of the domain \Omega=(-L,L)
+L=10;
+
+%%% Number of discrete points, mesh and mesh size
+Nval=[50,100,200,400,800,1600,3200,6400];
+step=[];
+dif_norm=[];
+dif_bilinear=[];
+dif_linfinity=[];
+
+pendiente=[];
+alpha=[];
+
+
+for N=Nval
+
+    xi=linspace(-L,L,N+2);
+    h=xi(2)-xi(1);
+
+    Alog=LoglapRigidity(L,N);
+    mass = MassMatrix(xi,h);
+
+    tabname="file"+num2str(N)+"_lambda10.txt";
+
+    T=readtable(tabname);
+    T=table2array(T);
+
+
+    %%% Extracting data from tables
+
+    %%% Right-hand side of the problem and projection over finite elements
+    %f = @(x) 1+0*x; %%% Torsion
+    %f = @(x) (-3*x.^2+1)+(log(1./(1-x.^2))+(2*log(2)+psi(1/2)+psi(1))).*(1-x.^2); %%% (1-x^2)
+    %f = @(x) log(1./(1^2-x.^2))+(2*log(2)+psi(1/2)+psi(1)); %%%
+    %characteristic
+   
+    %F=projection(xi,f,L,h);
+
+    f=T(:,2);
+    F=h*f; F(1)=F(1)*5/3; F(end)=F(end)*5/3;
+
+
+
+    exsol=@(x) 1./sqrt(-log((L^2-x.^2)/(2*L^2)));
+
+    %exsol=@(x) 1./sqrt(-log((L^2-x.^2)/(2)));
+
+    sol_log=(Alog)\F;
+
+    temp_err=exsol(xi)'-sol_log;
+    temp_err=temp_err(2:end-1); %%Removiendo el ultimo punto
+    %temp_err=temp_err(floor((N+1)/4):floor(3*(N+1)/4),1);
+
+    error_norm=E_norm(xi(2:end-1),temp_err,L);
+    err_linf=norm(exsol(xi)'-sol_log,"inf");
+
+    dif_norm=[dif_norm;error_norm];
+    dif_linfinity=[dif_linfinity;err_linf];
+    
+    step=[step;h];
+
+    if length(dif_norm)== 1
+        pendiente=[pendiente,NaN];
+        alpha=[alpha,NaN];
+    else
+        pendiente=[pendiente,log(dif_norm(end)/dif_norm(end-1))/log(step(end)/step(end-1))];
+        alpha=[alpha,abs(log(dif_norm(end)/dif_norm(end-1)))/abs(log(abs(log(step(end))/abs(log(step(end-1))))))];
+    end
+
+    figure(1)
+    %plot(xi, sol_log,xi,exsol(xi),'x',xi,sol_log*0,'LineWidth',1);
+    plot(xi, sol_log,xi,exsol(xi),'x','LineWidth',2);
+    
+end
+
+ figure(2)
+ loglog(step,dif_linfinity,step,0.33./(-log(step)).^(0.35),'LineWidth',2.5);
+ sl_dif=log(dif_linfinity(1)/dif_linfinity(end))/log(step(1)/step(end));
+ legend("Slope: "+num2str(sl_dif)); title('error L_inf')
+
+ figure(3)
+ %loglog(step,dif_norm,step,0.4./(-log(step)).^(7),'LineWidth',2.5);
+ loglog(step,dif_norm,'LineWidth',2.5);
+ sl_quad=log(dif_norm(1)/dif_norm(end))/log(step(1)/step(end));
+ legend("Slope: "+num2str(sl_quad)); title('error H quadrature')
+
+
+%%% Auxiliary functions
+
+function [F] = projection(xi,f,L,h)
+
+N=size(xi,2);
+Phi = @(x) 1-abs(x);
+F = zeros(N,1);
+
+for i=1:N
+    if i~=1 && i~=N
+        xx = linspace(xi(i)-h,xi(i)+h,N+1);
+        xx = 0.5*(xx(2:end)+xx(1:end-1));
+        B1 = f(xx).*Phi((xx-xi(i))/h);
+        F(i) = ((2*h)/N)*sum(B1);
+    end
+
+    if i==1
+        xx = linspace(xi(i),xi(i)+h,N+1);
+        xx = 0.5*(xx(2:end)+xx(1:end-1));
+        B1 = 2*f(xx).*Phi((xx-xi(i))/h);
+        F(i) = ((h)/N)*sum(B1);
+    end
+
+    if i==N
+        xx = linspace(xi(i)-h,xi(i),N+1);
+        xx = 0.5*(xx(2:end)+xx(1:end-1));
+        B1 = 2*f(xx).*Phi((xx-xi(i))/h);
+        F(i) = ((h)/N)*sum(B1);
+    end
+
+end
+
+end
+
+function A = LoglapRigidity(L,N)
+
+x = linspace(-L,L,N+2);
+h = x(2)-x(1);
+
+A = zeros(N+2,N+2);
+emc=-psi(1); %%Euler-Mascheroni constant
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Filling the upper triangle of FEM matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% Entry a(\phi_0,\phi_j) with j=2
+A(1,3)=h*(-2/3);
+A(N+2,N)=A(1,3);
+
+%%% Entry a(\phi_0,\phi_j) with j\in\inter{3,N}
+for j=4:N+1
+    A(1,j)=h*(1/3*(-3*j^3*log(j)+6*j^2*log(j)+(j-2)^3*(-log(j-2))...
+            +3*(j-1)^2*(j-2)*log(j-1)+(j+1)^2*(j-2)*log(j+1)-2));
+    A(N+2,N+3-j)= A(1,j);
+end
+
+%%% Entry a(\phi_0,\phi_j) with j=N+1
+A(1,N+2)= h*(2/3*(2*(N-3)*N^2*log(N)-N-(N-1)^3*log(N-1)-(N+1)*((N-4)*N+1)*log(N+1)-3));
+
+
+%%% Case: inner upper triangle 
+
+for i=2:N-1
+    for j=i+2:N+1
+        k = j-i;
+
+        if k~=2
+
+            PP= 1/6*h*(-(-2 + k)^3*log(-2 + k) + 4*(-1 + k)^3*log(-1 + k) - ...
+                6*k^3*log(k) + 4*(1 + k)^3*log(1 + k) - (2 + k)^3*log(2 + k));
+            A(i,j) = PP;
+        else
+            PPbis = -(1/3)*h*(-24*log(3) - 12*log(4) - 16*log(9) + 27*log(16) + log(144));
+            A(i,j) = PPbis;
+        end
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Filling the superdiagonal of FEM matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% Entry a(\phi_0,\phi_j) with j=1
+A(1,2)= h*(-emc/3+5/9-2/3*log(h)-2/3*log(2)+1/3*psi(1/2));
+A(N+2,N+1)=A(1,2);
+
+%%% Inner superdiagonal
+for i=2:N
+    A(i,i+1) = -(emc*h)/6 + (11*h)/18 - 1/3*h*log(h) ...
+        + 1/12*h*(64*log(2) - 54*log(3)) + 1/3*h*log(2) + 1/6*h*psi(1/2);
+end
+
+A = A+A';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Filling the diagonal of FEM matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% Entry a(\phi_0,\phi_0) 
+
+A(1,1)=h*(-(4*emc)/3+32/9-8/3*log(h)+8/3*log(2)+4/3*psi(1/2));
+A(N+2,N+2)=A(1,1);
+
+for i=2:N+1
+    A(i,i) = -2/9*h*(6*log(h) + 3*emc - 11 + log(64) - 3*psi(1/2));
+end
+
+end
+
+
+function [out] = E_norm(xi,u,r)
+%N = length(u);
+%x = linspace(-r,r,N);
+eps=10e-8;
+%eps = 1e-3*(xi(2)-xi(1));
+yi = xi;
+[Ux, Uy] = meshgrid(u,u);
+[X,Y] = meshgrid(xi,yi);
+
+W = 1/2*(Ux-Uy).^2./abs(X-Y);
+W(~isfinite(W)) = 0;
+Z = (abs(X-Y)<=1).*W;
+Z_new = zeros(length(Z),1);
+for i = 1:length(Z)
+    Z_new(i) = trapz(yi,Z(:,i));
+end
+out_1 = trapz(xi,Z_new);
+
+if r>= 1
+    killing_measure = ((r + eps - abs(xi))<=1).*(-log(r + eps -abs(xi)));
+else
+    killing_measure = ((abs(xi)-r)<1).*(-log((r+eps).^2 -abs(xi).^2))...
+                        + ((abs(xi)-r)>=1).*(-log(r+eps-abs(xi)));
+end
+
+
+out_2 = trapz(xi,u.^2.*killing_measure');
+out = out_1 + out_2;
+%out = out_2
+end
+
+function M = MassMatrix(x,hx)
+
+Nx = length(x);
+
+M=zeros(Nx,Nx);
+
+M(1,2)=1/3;
+M(Nx,Nx-1)=M(1,2);
+
+for i=2:Nx-2
+    M(i,i+1)=1/6;
+end
+
+M=M+M';
+
+M(1,1)=4/3;
+M(Nx,Nx)=M(1,1);
+ 
+for i=2:Nx-1
+    M(i,i)=2/3;
+end
+
+M = sparse(hx*M);
+
+end
+
+function [err] = error_bilinear(h,xi,sol,f)
+
+% numf=f(xi);
+% numf=numf(2:end-1);
+
+val = -1.32528;
+valnum = h*sum(f.*sol);
+err = val-valnum;
+
+end
+
+
