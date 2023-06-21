d6dd08c5a41b1c060f3f0b4e3e769e3f0e54b7a7
victor-santamaria <victor.santamaria@deusto.es>
2023-06-21 16:33:04 -0600

    updates some figures

--- Programas/FE_logarithmic_full_basis_noproj.m
+++ Programas/FE_logarithmic_full_basis_noproj.m
@@ -1,10 +1,10 @@
 clear
 
 %%% Size of the domain \Omega=(-L,L)
-L=3;
+L=1;
 
 %%% Number of discrete points, mesh and mesh size
-Nval=[50,100,200,400,800,1600];
+Nval=[200];
 step=[];
 dif_norm=[];
 dif_L2=[];
@@ -35,7 +35,7 @@ for N=Nval
 
 
 
-    tabname="./datos_sim/file"+num2str(N)+"_lambda3.txt";
+    tabname="./datos_sim/file"+num2str(N)+".txt";
 
     T=readtable(tabname);
     T=table2array(T);
@@ -111,8 +111,8 @@ end
 
 
  %%% Write otput files
- write_numsol_realsol(xi,sol_log,exsol(xi),exsol,false);
- write_convergence_data(xi,step,dif_L2,dif_L2_loc,dif_linfinity,slope_L2,slope_L2_loc,slope_inf,true)
+ write_numsol_realsol(xi,sol_log,exsol(xi),exsol,true);
+ write_convergence_data(xi,step,dif_L2,dif_L2_loc,dif_linfinity,slope_L2,slope_L2_loc,slope_inf,false)
 
  [xx,B0]=plt_sol_nearboundary(xi,sol_log,h);
 
